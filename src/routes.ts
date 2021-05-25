d4a8371284c01b1c34de67582b5cf43bbe09706a
lipk <mail@lipk.org>
2021-05-25 09:41:50 +0800

    stub webhook route

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,9 @@
-import { settings } from './app-settings';
-import { encodeState, tryDecodeState } from './state';
-import { addCorsHeaders } from './cors';
+import { settings } from "./app-settings";
+import { encodeState, tryDecodeState } from "./state";
+import { addCorsHeaders } from "./cors";
 
-const authorizeUrl = 'https://github.com/login/oauth/authorize';
-const accessTokenUrl = 'https://github.com/login/oauth/access_token';
+const authorizeUrl = "https://github.com/login/oauth/authorize";
+const accessTokenUrl = "https://github.com/login/oauth/access_token";
 
 export async function processRequest(fetchEvent: FetchEvent) {
   const response = await routeRequest(fetchEvent);
@@ -15,77 +15,99 @@ async function routeRequest(fetchEvent: FetchEvent) {
   const request = fetchEvent.request;
   const { origin, pathname, searchParams: search } = new URL(request.url);
 
-  if (request.method === 'OPTIONS') {
-    const response = new Response(undefined, { status: 200, statusText: 'OK' });
-    addCorsHeaders(response, settings.origins, request.headers.get('origin'));
+  if (request.method === "OPTIONS") {
+    const response = new Response(undefined, { status: 200, statusText: "OK" });
+    addCorsHeaders(response, settings.origins, request.headers.get("origin"));
     return response;
-  } else if (request.method === 'GET' && (pathname === '' || pathname === '/')) {
-    return new Response('alive', {
+  } else if (
+    request.method === "GET" &&
+    (pathname === "" || pathname === "/")
+  ) {
+    return new Response("alive", {
       status: 200,
-      statusText: 'OK',
-      headers: { 'Content-Type': 'text/plain' }
+      statusText: "OK",
+      headers: { "Content-Type": "text/plain" },
     });
-  } else if (request.method === 'GET' && pathname === '/authorize') {
+  } else if (request.method === "GET" && pathname === "/authorize") {
     return authorizeRequestHandler(origin, search);
-  } else if (request.method === 'GET' && pathname === '/authorized') {
+  } else if (request.method === "GET" && pathname === "/authorized") {
     return await authorizedRequestHandler(search);
-  } else if (request.method === 'POST' && pathname === '/token') {
+  } else if (request.method === "POST" && pathname === "/token") {
     const response = await tokenRequestHandler(request);
-    addCorsHeaders(response, settings.origins, request.headers.get('origin'));
+    addCorsHeaders(response, settings.origins, request.headers.get("origin"));
     return response;
-  } else if (request.method === 'POST' && /^\/repos\/[\w-]+\/[\w-.]+\/issues$/i.test(pathname)) {
-    const response = await postIssueRequestHandler(pathname, search, fetchEvent);
-    addCorsHeaders(response, settings.origins, request.headers.get('origin'));
+  } else if (
+    request.method === "POST" &&
+    /^\/repos\/[\w-]+\/[\w-.]+\/issues$/i.test(pathname)
+  ) {
+    const response = await postIssueRequestHandler(
+      pathname,
+      search,
+      fetchEvent
+    );
+    addCorsHeaders(response, settings.origins, request.headers.get("origin"));
     return response;
+  } else if (request.method === "POST" && pathname === "/webhook") {
+    return await webhookRequestHandler(fetchEvent);
   } else {
     return new Response(`Not Found: ${pathname}`, {
       status: 404,
-      statusText: 'not found',
-      headers: { 'Content-Type': 'text/plain' }
+      statusText: "not found",
+      headers: { "Content-Type": "text/plain" },
     });
   }
 }
 
 function applySecurityPolicy(response: Response) {
   // pages are not allowed to be shown in iframes.
-  response.headers.append('X-Frame-Options', 'DENY');
+  response.headers.append("X-Frame-Options", "DENY");
   // pages do not have any cross-origin deps on scripts or css.
-  response.headers.append('Content-Security-Policy', `default-src 'self'`);
+  response.headers.append("Content-Security-Policy", `default-src 'self'`);
   // don't cache responses.
-  response.headers.append('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  response.headers.append(
+    "Cache-Control",
+    "no-store, no-cache, must-revalidate, proxy-revalidate"
+  );
 }
 
 function badRequest(message: string) {
   return new Response(message, {
     status: 400,
-    statusText: 'bad request',
-    headers: { 'Content-Type': 'text/plain' }
+    statusText: "bad request",
+    headers: { "Content-Type": "text/plain" },
   });
 }
 
-async function authorizeRequestHandler(origin: string, search: URLSearchParams) {
+async function authorizeRequestHandler(
+  origin: string,
+  search: URLSearchParams
+) {
   const { client_id, state_password } = settings;
 
-  const appReturnUrl = search.get('redirect_uri');
+  const appReturnUrl = search.get("redirect_uri");
 
   if (!appReturnUrl) {
     return badRequest(`"redirect_uri" is required.`);
   }
 
   const state = await encodeState(appReturnUrl, state_password);
-  const redirect_uri = origin + '/authorized';
+  const redirect_uri = origin + "/authorized";
   return new Response(undefined, {
     status: 302,
-    statusText: 'found',
+    statusText: "found",
     headers: {
-      Location: `${authorizeUrl}?${new URLSearchParams({ client_id, redirect_uri, state })}`
-    }
+      Location: `${authorizeUrl}?${new URLSearchParams({
+        client_id,
+        redirect_uri,
+        state,
+      })}`,
+    },
   });
 }
 
 async function authorizedRequestHandler(search: URLSearchParams) {
-  const code = search.get('code');
-  const state = search.get('state');
+  const code = search.get("code");
+  const state = search.get("state");
 
   if (!code) {
     return badRequest('"code" is required.');
@@ -103,13 +125,18 @@ async function authorizedRequestHandler(search: URLSearchParams) {
   }
 
   const init = {
-    method: 'POST',
-    body: (new URLSearchParams({ client_id, client_secret, code, state })).toString(),
+    method: "POST",
+    body: new URLSearchParams({
+      client_id,
+      client_secret,
+      code,
+      state,
+    }).toString(),
     headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'User-Agent': 'beaudar'
-    }
+      Accept: "application/json",
+      "Content-Type": "application/x-www-form-urlencoded",
+      "User-Agent": "beaudar",
+    },
   };
 
   let accessToken: string;
@@ -122,18 +149,22 @@ async function authorizedRequestHandler(search: URLSearchParams) {
       throw new Error(`Access token response had status ${response.status}.`);
     }
   } catch (_) {
-    return new Response('Unable to load token from GitHub.');
+    return new Response("Unable to load token from GitHub.");
   }
 
   const url = new URL(returnUrl);
-  const session = await encodeState(accessToken, state_password, Date.now() + 1000 * 60 * 60 * 24 * 365);
-  url.searchParams.set('beaudar', session);
+  const session = await encodeState(
+    accessToken,
+    state_password,
+    Date.now() + 1000 * 60 * 60 * 24 * 365
+  );
+  url.searchParams.set("beaudar", session);
   return new Response(undefined, {
     status: 302,
-    statusText: 'found',
+    statusText: "found",
     headers: {
-      'Location': url.href
-    }
+      Location: url.href,
+    },
   });
 }
 
@@ -143,7 +174,7 @@ async function tokenRequestHandler(request: Request) {
   try {
     session = await request.json();
   } catch {
-    return badRequest('Unable to parse body');
+    return badRequest("Unable to parse body");
   }
 
   const token = await tryDecodeState(session, state_password);
@@ -154,74 +185,89 @@ async function tokenRequestHandler(request: Request) {
 
   return new Response(JSON.stringify(token), {
     status: 200,
-    statusText: 'OK',
+    statusText: "OK",
     headers: {
-      'Content-Type': 'application/json'
-    }
+      "Content-Type": "application/json",
+    },
   });
 }
 
-async function postIssueRequestHandler(path: string, search: URLSearchParams, fetchEvent: FetchEvent) {
+async function postIssueRequestHandler(
+  path: string,
+  search: URLSearchParams,
+  fetchEvent: FetchEvent
+) {
   const request = fetchEvent.request;
-  const authorization = request.headers.get('authorization');
+  const authorization = request.headers.get("authorization");
 
   if (!authorization) {
-    return badRequest('Authorization header is required.');
+    return badRequest("Authorization header is required.");
   }
 
   const authInit = {
-    method: 'GET',
+    method: "GET",
     headers: {
-      'Authorization': authorization,
-      'User-Agent': 'beaudar'
-    }
+      Authorization: authorization,
+      "User-Agent": "beaudar",
+    },
   };
 
   let authenticated = false;
   try {
-    const response = await fetch('https://api.github.com/user', authInit);
+    const response = await fetch("https://api.github.com/user", authInit);
     authenticated = response.ok;
-  } catch (_) {
-  }
+  } catch (_) {}
   if (!authenticated) {
-    return new Response(undefined, { status: 401, statusText: 'not authorized' });
+    return new Response(undefined, {
+      status: 401,
+      statusText: "not authorized",
+    });
   }
 
   const init = {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Authorization': 'token ' + settings.bot_token,
-      'User-Agent': 'beaudar',
+      Authorization: "token " + settings.bot_token,
+      "User-Agent": "beaudar",
     },
-    body: request.body
+    body: request.body,
   };
   try {
     const response = await fetch(`https://api.github.com${path}`, init);
     const issue = await response.json();
-    if (search.has('label')) {
+    if (search.has("label")) {
       const labelsInit = {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Authorization': authorization,
-          'User-Agent': 'beaudar',
-          'Accept': 'application/vnd.github.symmetra-preview+json'
+          Authorization: authorization,
+          "User-Agent": "beaudar",
+          Accept: "application/vnd.github.symmetra-preview+json",
         },
-        body: JSON.stringify({ labels: [search.get('label')] })
+        body: JSON.stringify({ labels: [search.get("label")] }),
       };
-      fetchEvent.waitUntil(fetch(`https://api.github.com${path}/${issue.number}/labels`, labelsInit));
+      fetchEvent.waitUntil(
+        fetch(
+          `https://api.github.com${path}/${issue.number}/labels`,
+          labelsInit
+        )
+      );
     }
     return new Response(JSON.stringify(issue), {
       status: response.status,
       statusText: response.statusText,
       headers: {
-        'Content-Type': 'application/json'
-      }
+        "Content-Type": "application/json",
+      },
     });
   } catch (_) {
-    return new Response('Unable to post issue to GitHub.', {
+    return new Response("Unable to post issue to GitHub.", {
       status: 503,
-      statusText: 'service unavailable',
-      headers: { 'Content-Type': 'text/plain' }
+      statusText: "service unavailable",
+      headers: { "Content-Type": "text/plain" },
     });
   }
+}
+
+async function webhookRequestHandler(_: FetchEvent) {
+  return new Response(undefined, { status: 204 });
 }
